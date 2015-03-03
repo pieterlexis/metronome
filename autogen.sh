f3f318f2f48ec645178126e15a45fd9291b0c62a
Pieter Lexis <pieter@plexis.eu>
2015-03-03 16:29:51 +0100

    autoconf nonmagic

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-aclocal -Im4
-autoheader
-autoconf
-automake -ac
+dirs='src/yahttp .'
+
+do_gen() {
+  cd $1
+  aclocal -Im4
+  autoheader
+  autoconf
+  automake -ac
+}
+
+for dir in $dirs; do
+  do_gen $dir
+done
